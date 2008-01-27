409aaadc330fc29eb7c06b621d9de1ae70b1824d
Adam Murray <adamjmurray@gmail.com>
2008-01-27 05:21:16 +0000

    more/improved ParserTest cases
    git-svn-id: svn://quagmire/repo/max/externals/java/src/ajm@43 99bd3f52-245c-3848-8005-63e5fbd5b414

--- util/ParserTest.java
+++ util/ParserTest.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import junit.framework.TestCase;
-
 import ajm.data.Item;
 
 import com.cycling74.max.Atom;
@@ -55,6 +54,11 @@ public class ParserTest extends TestCase {
 		return atoms;
 	}
 
+
+	/*------------------------------------------------
+	 *  The Tests
+	 *------------------------------------------------*/
+
 	public void testEmpty() {
 		List<Atom> eval = toAtomList(p.parse(""));
 		assertEquals(eval.size(), 0);
@@ -63,115 +67,174 @@ public class ParserTest extends TestCase {
 
 	public void testSingleString() {
 		List<Atom> eval = toAtomList(p.parse("hello"));
-		assertEquals(eval.size(), 1);
-		assertEquals(eval, toAtomList("hello"));
+		assertEquals(1, eval.size());
+		assertEquals(toAtomList("hello"), eval);
 	}
 
 	public void testMultipleString() {
 		List<Atom> eval = toAtomList(p.parse("hello world"));
-		assertEquals(eval.size(), 2);
-		assertEquals(eval, toAtomList("hello", "world"));
+		assertEquals(2, eval.size());
+		assertEquals(toAtomList("hello", "world"), eval);
 		eval = toAtomList(p.parse("   hello    world    "));
-		assertEquals(eval, toAtomList("hello", "world"));
+		assertEquals(toAtomList("hello", "world"), eval);
 		eval = toAtomList(p.parse("a b c d e f ggggg"));
-		assertEquals(eval.size(), 7);
-		assertEquals(eval, toAtomList("a", "b", "c", "d", "e", "f", "ggggg"));
+		assertEquals(7, eval.size());
+		assertEquals(toAtomList("a", "b", "c", "d", "e", "f", "ggggg"), eval);
 	}
 
 	public void testInt() {
 		List<Atom> eval = toAtomList(p.parse("1"));
-		assertFalse(eval.equals(toAtomList("1")));
-		assertEquals(eval, toAtomList(1));
+		assertFalse(eval.equals(toAtomList("1"))); // an int is not a string
+		assertEquals(toAtomList(1), eval);
 		eval = toAtomList(p.parse("1 0 -1"));
-		assertEquals(eval, toAtomList(1, 0, -1));
+		assertEquals(toAtomList(1, 0, -1), eval);
 	}
 
 	public void testFloat() {
 		List<Atom> eval = toAtomList(p.parse("1.1"));
-		assertFalse(eval.equals(toAtomList("1.1")));
-		assertEquals(eval, toAtomList(1.1));
+		assertFalse(eval.equals(toAtomList("1.1"))); // a float is not a string
+		assertEquals(toAtomList(1.1), eval);
 		eval = toAtomList(p.parse("1.1 0.9 -1.333"));
-		assertEquals(eval, toAtomList(1.1, 0.9, -1.333));
+		assertEquals(toAtomList(1.1, 0.9, -1.333), eval);
 	}
 
 	public void testMixed() {
-		assertEquals(toAtomList(p.parse("hello 1 world 1.3 pthb")), toAtomList("hello", 1, "world", 1.3, "pthb"));
-		assertEquals(toAtomList(p.parse("hello   1  world     1.3 pthb")), toAtomList("hello", 1, "world", 1.3, "pthb"));
+		assertEquals(toAtomList("hello", 1, "world", 1.3, "pthb"), toAtomList(p.parse("hello 1 world 1.3 pthb")));
+		assertEquals(toAtomList("hello", 1, "world", 1.3, "pthb"), toAtomList(p.parse("hello   1  world     1.3 pthb")));
 	}
 
 	public void testBooleanIsString() {
-		assertEquals(toAtomList(p.parse("true")), toAtomList("true"));
+		assertEquals(toAtomList("true"), toAtomList(p.parse("true")));
 	}
 
-	public void testSingleRepetition() {
-		assertEquals(toAtomList(p.parse("1*2")), toAtomList(1, 1));
-		assertEquals(toAtomList(p.parse("2 * 3")), toAtomList(2, 2, 2));
-		assertEquals(toAtomList(p.parse("(3)*4")), toAtomList(3, 3, 3, 3));
-		assertEquals(toAtomList(p.parse("(3)* 4")), toAtomList(3, 3, 3, 3));
-		assertEquals(toAtomList(p.parse("(3) *4")), toAtomList(3, 3, 3, 3));
-		assertEquals(toAtomList(p.parse("(3) * 4")), toAtomList(3, 3, 3, 3));
+
+	public void testDegenerateRepetition() {
+		assertEquals(toAtomList(1, 2, 3), toAtomList(p.parse("(1 2 3)")));
 	}
+
+	public void testSingleValueRepetition() {
+		assertEquals(toAtomList(1, 1), toAtomList(p.parse("1*2")));
+		assertEquals(toAtomList(2, 2, 2), toAtomList(p.parse("2 * 3")));
+		assertEquals(toAtomList(3, 3, 3, 3), toAtomList(p.parse("(3)*4")));
+		assertEquals(toAtomList(3, 3, 3, 3), toAtomList(p.parse("(3)* 4")));
+		assertEquals(toAtomList(3, 3, 3, 3), toAtomList(p.parse("(3) *4")));
+		assertEquals(toAtomList(3, 3, 3, 3), toAtomList(p.parse("(3) * 4")));
+	}
+
+	public void testMultiValueRepetition() {
+		assertEquals(toAtomList(1, 2, 3, 1, 2, 3), toAtomList(p.parse("(1 2 3)*2")));
+		assertEquals(toAtomList(1, 2, 1, 2, 1, 2), toAtomList(p.parse("(1 2)*3")));
+		assertEquals(toAtomList(1, 2, 1, 2, 1, 2), toAtomList(p.parse("(1 2) * 3")));
+	}
+
 
 	public void testMultipleRepetition() {
-		assertEquals(toAtomList(p.parse("1*2 2*3 (3)*4")), toAtomList(1, 1, 2, 2, 2, 3, 3, 3, 3));
-		assertEquals(toAtomList(p.parse("1 *2 2* 3 (3) * 4")), toAtomList(1, 1, 2, 2, 2, 3, 3, 3, 3));
-		assertEquals(toAtomList(p.parse("1*2 2   *    3 (3)*4")), toAtomList(1, 1, 2, 2, 2, 3, 3, 3, 3));
+		assertEquals(toAtomList(1, 1, 2, 2, 2, 3, 3, 3, 3), toAtomList(p.parse("1*2 2*3 (3)*4")));
+		assertEquals(toAtomList(1, 1, 2, 2, 2, 3, 3, 3, 3), toAtomList(p.parse("1 *2 2* 3 (3) * 4")));
+		assertEquals(toAtomList(1, 1, 2, 2, 2, 3, 3, 3, 3), toAtomList(p.parse("1*2 2   *    3 (3)*4")));
 	}
 
-	public void testRepetitionGroup() {
-		assertEquals(toAtomList(p.parse("(1 2)*3")), toAtomList(1, 2, 1, 2, 1, 2));
-		assertEquals(toAtomList(p.parse("(1 2) * 3")), toAtomList(1, 2, 1, 2, 1, 2));
+	public void testNonRepeats() {
+		assertEquals(toAtomList(), toAtomList(p.parse("1*0")));
+		assertEquals(toAtomList(), toAtomList(p.parse("(1 2 3)*0")));
+		assertEquals(toAtomList(), toAtomList(p.parse("[1 2 3]*0")));
 	}
 
 	public void testNestedRepetition() {
-		assertEquals(toAtomList(p.parse("(1 2*2 3)*2")), toAtomList(1, 2, 2, 3, 1, 2, 2, 3));
+		assertEquals(toAtomList(1, 2, 2, 3, 1, 2, 2, 3), toAtomList(p.parse("(1 2*2 3)*2")));
+		assertEquals(toAtomList(1, 2, 3, 2, 3, 2, 3, 1, 2, 3, 2, 3, 2, 3), toAtomList(p.parse("(1 (2 3)*3)*2")));
+		assertEquals(toAtomList(1, 2, 2, 3, 2, 2, 3, 1, 2, 2, 3, 2, 2, 3), toAtomList(p.parse("(1 (2*2 3)*2)*2")));
 	}
 
+
+	public void testNextPrev() {
+		assertEquals(toAtomList("next", "prev", "next", "next"), toAtomList(p.parse("> < >>")));
+	}
+
+
+	public void testNote() {
+		assertEquals(toAtomList(0), toAtomList(p.parse("C-1")));
+		assertEquals(toAtomList(12), toAtomList(p.parse("C0")));
+		assertEquals(toAtomList(24), toAtomList(p.parse("C1")));
+		assertEquals(toAtomList(60), toAtomList(p.parse("C4")));
+		assertEquals(toAtomList(62), toAtomList(p.parse("D4")));
+		assertEquals(toAtomList(64), toAtomList(p.parse("E4")));
+		assertEquals(toAtomList(65), toAtomList(p.parse("F4")));
+		assertEquals(toAtomList(67), toAtomList(p.parse("G4")));
+		assertEquals(toAtomList(69), toAtomList(p.parse("A4")));
+		assertEquals(toAtomList(71), toAtomList(p.parse("B4")));
+		assertEquals(toAtomList(0), toAtomList(p.parse("c-1")));
+		assertEquals(toAtomList(12), toAtomList(p.parse("c0")));
+		assertEquals(toAtomList(24), toAtomList(p.parse("c1")));
+		assertEquals(toAtomList(60), toAtomList(p.parse("c4")));
+		assertEquals(toAtomList(62), toAtomList(p.parse("d4")));
+		assertEquals(toAtomList(64), toAtomList(p.parse("e4")));
+		assertEquals(toAtomList(65), toAtomList(p.parse("f4")));
+		assertEquals(toAtomList(67), toAtomList(p.parse("G4")));
+		assertEquals(toAtomList(69), toAtomList(p.parse("a4")));
+		assertEquals(toAtomList(71), toAtomList(p.parse("b4")));
+	}
+
+	public void testNoteAccidental() {
+		assertEquals(toAtomList(61), toAtomList(p.parse("C#4")));
+		assertEquals(toAtomList(61), toAtomList(p.parse("Db4")));
+		assertEquals(toAtomList(62), toAtomList(p.parse("C##4")));
+		assertEquals(toAtomList(60), toAtomList(p.parse("Dbb4")));
+		assertEquals(toAtomList(65), toAtomList(p.parse("e#4")));
+		assertEquals(toAtomList(64), toAtomList(p.parse("fb4")));
+		assertEquals(toAtomList(66), toAtomList(p.parse("e##4")));
+		assertEquals(toAtomList(63), toAtomList(p.parse("fbb4")));
+	}
+
+	public void testNoteSequence() {
+		assertEquals(toAtomList(60, 64, 67, 72), toAtomList(p.parse("C4 e4 G4 b#4")));
+		assertEquals(toAtomList(60, 67, 64, 72), toAtomList(p.parse("C4 G4 e4 b#4")));
+	}
+
+	public void testNoteRepeat() {
+		assertEquals(toAtomList(60, 64, 67, 72, 60, 64, 67, 72), toAtomList(p.parse("(C4 e4 G4 b#4)*2")));
+		assertEquals(toAtomList(60, 64, 67, 64, 67, 72), toAtomList(p.parse("C4 (e4 G4)*2 b#4")));
+		assertEquals(toAtomList(60, 64, 67, 67, 72), toAtomList(p.parse("C4 e4 G4*2 b#4")));
+	}
+
+
 	public void testChord() {
-		assertEquals(toAtomList(p.parse("[1 2 3]")), toAtomList("1 2 3"));
-		assertEquals(toAtomList(p.parse(" [ 1 2 3   ]")), toAtomList("1 2 3"));
+		assertEquals(toAtomList("1 2 3"), toAtomList(p.parse("[1 2 3]")));
+		assertEquals(toAtomList("1 2 3"), toAtomList(p.parse(" [ 1 2 3   ]")));
 	}
 
 	public void testMixedChord() {
+		assertEquals(toAtomList("1 2 3", "a b"), toAtomList(p.parse("[1 2 3] [a b]")));
 		assertEquals(toAtomList(p.parse("[1 1.1 a >]")), toAtomList("1 1.1 a next"));
-
 	}
 
-	// TODO: all the note name parsing
-
-	public void testNextPrev() {
-		assertEquals(toAtomList(p.parse("> < >>")), toAtomList("next", "prev", "next", "next"));
-	}
-
-	public void testGetChordMapSingle() {
-		assertEquals(toAtomList(p.parse("[1 2 3]")), toAtomList("1 2 3"));
-
-		/* TODO: get the original Item list and make sure things look good!
-		Map<Integer, Item> chords = p.getChords();
-		assertEquals(chords.size(), 1);
-		assertEquals(chords.get(0), toItem(1, 2, 3));
-		*/
-	}
-
-	public void testGetChordMapMultiple() {
-		assertEquals(toAtomList(p.parse("[1 2 3] [a b]")), toAtomList("1 2 3", "a b"));
-		/*
-		Map<Integer, Item> chords = p.getChords();
-		assertEquals(chords.size(), 2);
-		assertEquals(chords.get(0), toItem(1, 2, 3));
-		assertEquals(chords.get(1), toItem("a", "b"));
-		*/
-	}
-
-	// TODO: all the above methods got expected/actual backwards
-	public void testGetChordMapRepetition() {
+	public void testChordRepetition() {
+		assertEquals(toAtomList("1 2 3", "1 2 3"), toAtomList(p.parse("[1 2 3]*2")));
 		assertEquals(toAtomList("1 2 2", "3 4", "3 4"), toAtomList(p.parse("[1 2*2] [3 4]*2")));
-		/*
-		Map<Integer, Item> chords = p.getChords();
-		assertEquals(3, chords.size());
-		assertEquals(toItem(1, 2, 2), chords.get(0));
-		assertEquals(toItem(3, 4), chords.get(1));
-		assertEquals(toItem(3, 4), chords.get(2));
-		*/
+		assertEquals(toAtomList(1, "2 3", "2 3", 1, "2 3", "2 3"), toAtomList(p.parse("(1 [2 3]*2)*2")));
+		assertEquals(toAtomList(1, "2 3", "2 3", 1, "2 3", "2 3"), toAtomList(p.parse("(1 ([2 3])*2)*2")));
+		assertEquals(toAtomList(1, "2 3", "2 3", 1, "2 3", "2 3"), toAtomList(p.parse("(1 [  2  3  ]* 2) *  2")));
+		assertEquals(toAtomList(1, "2 3", "2 3", 1, "2 3", "2 3"), toAtomList(p.parse("(1 (  [  2  3  ])* 2) *  2")));
+		assertEquals(toAtomList(1, "2 2 3", "2 2 3", 1, "2 2 3", "2 2 3"), toAtomList(p.parse("(1 [2*2 3]*2)*2")));
+		assertEquals(toAtomList(1, 2, 3, 1, 2, 3, 1, 1, "1 2 3", "1 2 3"), toAtomList(p
+				.parse("(1 2 3)*2 1*2 [1 2 3]*2")));
 	}
+
+
+	public void testNoteChords() {
+		assertEquals(toAtomList("60 67 64 72"), toAtomList(p.parse("[C4 G4 e4 b#4]")));
+		assertEquals(toAtomList(60, "67 64", 72), toAtomList(p.parse("C4 [G4 e4] b#4")));
+	}
+
+	public void testNoNestedChords() {
+		try {
+			p.parse("[1 [2 3]]");
+			fail("Invalid syntax parsed successfully");
+		}
+		catch (IllegalStateException e) {
+			// should make it here
+		}
+	}
+
+	// TODO: other bad syntax
 }

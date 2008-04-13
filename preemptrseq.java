9a8022368cc9c6e8908379f5b3603a53f5156b7c
Adam Murray <adamjmurray@gmail.com>
2008-04-13 04:01:40 +0000

    added preemptive rseq for sequencing pattrstorage
    git-svn-id: svn://quagmire/repo/max/externals/java/src/ajm@125 99bd3f52-245c-3848-8005-63e5fbd5b414

---
+++ preemptrseq.java
@@ -0,0 +1,66 @@
+package ajm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ajm.data.Item;
+
+import com.cycling74.max.Atom;
+
+public class preemptrseq extends rseq {
+
+	List<Item> actualSeq = new ArrayList<Item>();
+
+	public preemptrseq(Atom[] args) {
+		super(args);
+	}
+
+	@Override
+	public void set(Atom[] list) {
+		try {
+			List<Item> newSeq = parser.parse(list);
+
+			actualSeq.clear();
+			actualSeq.addAll(newSeq);
+			setPreemptiveSeq();
+
+			onSeqChange();
+		}
+		catch (IllegalStateException e) {
+			err("Could not evaluate: " + toString(list) + "\n" + e.getMessage());
+		}
+	}
+
+	// The seq needs to look the same externally
+	// but internally transform positive numbers n into -(n-1), -1
+	// e.g. 16 4 3 -> -15 1 -3 1 -2 1
+	private void setPreemptiveSeq() {
+		seq.clear();
+		for (Item item : actualSeq) {
+			Atom atom = item.getAtom();
+			if (isNumber(atom)) {
+				int val = atom.toInt();
+				if (val > 0) {
+					seq.add(new Item(-(val - 1)));
+					seq.add(new Item(1));
+				}
+				else {
+					seq.add(item);
+				}
+			}
+			else {
+				seq.add(item);
+			}
+		}
+	}
+
+	@Override
+	public Atom[] getseq() {
+		Atom[] atoms = new Atom[actualSeq.size()];
+		for (int i = 0; i < actualSeq.size(); i++) {
+			atoms[i] = actualSeq.get(i).getAtom();
+		}
+		return atoms;
+	}
+
+}
